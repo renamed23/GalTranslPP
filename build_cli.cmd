3304f7d0e0ab3218cfb2b1cea4f1511b44a35987
renamed23 <savinpeoplefrombox@qq.com>
2026-04-04 13:10:12 +0800

    r

--- build_cli.cmd
+++ build_cli.cmd
@@ -2,7 +2,7 @@
 chcp 65001>nul
 
 :: 加载 VS 环境
-echo [1/4] 正在加载 VS 编译环境...
+echo [1/3] 正在加载 VS 编译环境...
 call "C:\Program Files\Microsoft Visual Studio\18\Community\VC\Auxiliary\Build\vcvars64.bat"
 
 :: 检查上一步是否成功，如果找不到文件直接报错
@@ -12,28 +12,12 @@ if %errorlevel% neq 0 (
     exit /b
 )
 
-:: 编译 OpenCC (切换目录并执行)
-echo [2/4] 正在编译 OpenCC...
-if exist "3rdParty\OpenCC\" (
-    pushd "3rdParty\OpenCC\"
-    uv run cmake -S . -B out -DCMAKE_INSTALL_PREFIX:PATH="%cd%\3rdParty\OpenCC\out\install\x64-Release"
-    uv run cmake --build out --config Release --target install
-    popd
-) else (
-    echo [错误] 找不到 OpenCC 目录。
-    pause
-    exit /b
-)
-
 :: 编译
-echo [3/4] 正在编译 GPPCLI 及其依赖项...
+echo [2/3] 正在编译 GPPCLI 及其依赖项...
 msbuild "GalTranslPP.slnx" /t:GPPCLI /p:Configuration=Release /p:Platform=x64 /p:CL_MPCount=0
 
 :: 复制资产文件
-echo [4/4] 正在复制资产文件到 Release 目录...
-xcopy "Example\BaseConfig" "Release\GPPCLI\BaseConfig" /E /I /Y
-xcopy "Example\sampleProject" "Release\GPPCLI\sampleProject" /E /I /Y
-xcopy "3rdParty\7z.dll" "Release\GPPCLI" /Y
-
+echo [3/3] 正在复制资产文件到 Release 目录...
+call "Release.bat"
 echo 完成
 pause>nul
